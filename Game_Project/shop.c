8f79f41491f338c71fff9037d0b278adbce744c6
Dan <ngdanck@gmail.com>
2025-10-16 14:13:57 +0800

    Updated shop to link with money script

--- Game_Project/shop.c
+++ Game_Project/shop.c
@@ -7,13 +7,18 @@
 // Required to get power level of sponge
 #include "sponge.h"
 
+// Handles transactions
+#include "money.h"
+
 // X and Y position of the shop menu (temporary)
 float x_pos = 1400, y_pos = 300;
 int shopToggle = 0;
 
 char currentLvlText[20];
+char upgradeText[20];
 
-
+int upgradeCost = 5;
+int increment = 1;
 
 void upgrade_sponge_button(void) {
 	// Upgrade button
@@ -24,13 +29,23 @@ void upgrade_sponge_button(void) {
 	CP_Settings_TextSize(30.0f);
 
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
-	CP_Font_DrawTextBox("Upgrade", x_pos - 50, y_pos + 200, 100);
+	sprintf_s(upgradeText, sizeof(upgradeText), "Upgrade Cost : %d", upgradeCost);
+	CP_Font_DrawTextBox(upgradeText, x_pos - 50, y_pos + 200, 100);
 
 
 	// TODO: Deduct money when upgrading sponge and also edge case to prevent upgrading if not enough money
 	if (CP_Input_MouseClicked()) {
 		if (IsAreaClicked(x_pos, y_pos + 200, 100, 100, CP_Input_GetMouseX(), CP_Input_GetMouseY())) {
-			upgrade_Sponge();
+			
+			// Check if player has enough money to upgrade sponge
+			if (get_current_money() >= upgradeCost) {
+				increment_decrement_money(-upgradeCost);
+				upgrade_Sponge();
+
+				// Increase cost for next upgrade
+				upgradeCost += increment;
+				increment++;
+			}
 		}
 	}
 }
