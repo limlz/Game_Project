5ab5fa19423a2017d6a2e8f6a2a1e0c2110f9bb9
limlz <62799391+limlz@users.noreply.github.com>
2025-10-19 00:00:30 +0800

    Added some interactivity to main menu buttons

--- Game_Project/mainmenu.c
+++ Game_Project/mainmenu.c
@@ -23,20 +23,37 @@ void Main_Menu_Init(void)
 
 void Main_Menu_Update(void)
 {
+
+	int play_pop = 0, exit_pop = 0, settings_pop = 0;
+	// Get mouse x y
+	mx = CP_Input_GetMouseX();
+	my = CP_Input_GetMouseY();
+
 	CP_Graphics_ClearBackground(CP_Color_Create(233, 239, 255, 255));
 
 	float center_x = CP_System_GetWindowWidth() * 0.5f;
 	float button_y = CP_System_GetWindowHeight() * 0.5f;
 
+	// Interactive aspect of buttons
+	if (IsAreaClicked(center_x, button_y - OFFSET + MOVE_DOWN, 300, 150, mx, my)) {
+		play_pop = 10;
+	}
+	else if (IsAreaClicked(center_x, button_y + OFFSET + MOVE_DOWN, 300, 150, mx, my)) {
+		exit_pop = 10;
+	}
+	else if (IsAreaClicked(120, CP_System_GetWindowHeight() - 120, 100, 100, mx, my)) {
+		settings_pop = 10;
+	}
+
 	// Draw rectangles for buttons
 	CP_Settings_Fill(buttons);
 	CP_Settings_NoStroke();
-	CP_Graphics_DrawRectAdvanced(center_x, button_y + OFFSET + MOVE_DOWN, 300, 150, 0, 50);
-	CP_Graphics_DrawRectAdvanced(center_x, button_y - OFFSET + MOVE_DOWN, 300, 150, 0, 50);
+	CP_Graphics_DrawRectAdvanced(center_x, button_y - OFFSET + MOVE_DOWN, 300 + play_pop, 150 + play_pop, 0, 50);
+	CP_Graphics_DrawRectAdvanced(center_x, button_y + OFFSET + MOVE_DOWN, 300 + exit_pop, 150 + exit_pop, 0, 50);
 	CP_Settings_Stroke(CP_Color_Create(255, 255, 255, 255));
 
 	// Draw settings button
-	CP_Graphics_DrawRectAdvanced(120, CP_System_GetWindowHeight() - 120, 100, 100, 0, 20);
+	CP_Graphics_DrawRectAdvanced(120, CP_System_GetWindowHeight() - 120, 100 + settings_pop, 100 + settings_pop, 0, 20);
 	CP_Settings_Fill(button_text);
 	CP_Graphics_DrawCircle(120, CP_System_GetWindowHeight() - 120, 50);
 
@@ -55,8 +72,6 @@ void Main_Menu_Update(void)
 
 	// Check for which buttons are clicked
 	if (CP_Input_MouseClicked()) {
-		mx = CP_Input_GetMouseX();
-		my = CP_Input_GetMouseY();
 		if (IsAreaClicked(center_x, button_y - OFFSET + MOVE_DOWN, 300, 150, mx, my)) {
 			CP_Engine_SetNextGameState(Game_Init, Game_Update, Game_Exit);
 		}
