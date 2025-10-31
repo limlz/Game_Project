98041fcd188b57cb42a834615a78142f086d70b5
junwxn <chiujunwen02@gmail.com>
2025-10-31 13:22:55 +0800

    confetti

---
+++ Game_Project/confetti.c
@@ -0,0 +1,53 @@
+#include "confetti.h"
+#include "cprocessing.h"
+
+#define MAX_CONFETTI 1000
+
+typedef struct {
+	float x, y;
+	float vx, vy;
+	float rotation;
+	float size;
+	CP_Color color;
+	int active;
+} Confetti;
+
+static Confetti confetti[MAX_CONFETTI];
+
+void InitConfetti(void) {
+    for (int i = 0; i < MAX_CONFETTI; i++) {
+        confetti[i].x = CP_Random_RangeFloat(0, CP_System_GetWindowWidth());    //horizontal spawn location range
+        confetti[i].y = CP_Random_RangeFloat(-1000, 0);                         //vertical spawn location range
+        confetti[i].vx = CP_Random_RangeFloat(-50, 50);                         //horizontal velocity
+        confetti[i].vy = CP_Random_RangeFloat(100, 300);                        //vertical velocity
+        confetti[i].rotation = CP_Random_RangeFloat(0, 360); 
+        confetti[i].size = CP_Random_RangeFloat(4, 10); 
+        confetti[i].color = CP_Color_Create(
+            CP_Random_RangeInt(180, 255),
+            CP_Random_RangeInt(140, 255),
+            CP_Random_RangeInt(140, 255),
+            255
+        );
+        confetti[i].active = 1;
+    }
+}
+
+void UpdateConfetti(float dt) {
+    for (int i = 0; i < MAX_CONFETTI; i++) {
+        if (!confetti[i].active) continue;
+
+        confetti[i].y += confetti[i].vy * dt;                                   //confetti vertical movement
+        confetti[i].x += confetti[i].vx * dt;                                   //confetti horizontal movement
+        confetti[i].rotation += 360 * dt;                                       //confetti rotation
+        confetti[i].vy += 110 * dt;                                             //gravity      
+        confetti[i].vx *= 1.0f;                                                 //slow or speed up horizontal movement
+
+        if (confetti[i].y > CP_System_GetWindowHeight() + 20)
+            confetti[i].active = 0;
+
+        CP_Settings_Stroke(confetti[i].color);
+        CP_Settings_Fill(confetti[i].color);
+        CP_Graphics_DrawRectAdvanced(confetti[i].x, confetti[i].y,
+            confetti[i].size, confetti[i].size, confetti[i].rotation, 0.0f);
+    }
+}
---
+++ Game_Project/confetti.h
@@ -0,0 +1,3 @@
+#pragma once
+void InitConfetti(void);
+void UpdateConfetti(float dt);
