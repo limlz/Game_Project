9c005ec32c2f7161abfd0ac2954d0a63e5a4ec4f
junwxn <chiujunwen02@gmail.com>
2025-11-24 18:48:20 +0800

    comments and stuff

--- src/core/leaderboard.c
+++ src/core/leaderboard.c
@@ -60,22 +60,30 @@ static float mx, my;
 FILE* leaderboard_file;
 
 void Leaderboard_Init(void) {
+
+	//load assets
 	title_font = CP_Font_Load("Assets/SuperWater.ttf");
 	back_arrow = CP_Image_Load("Assets/back_arrow.png");
 	montserrat_light = CP_Font_Load("Assets/MontserratLight.ttf");
+
+	//initialise colors
 	black = CP_Color_Create(0, 0, 0, 255);
 	button_blue = CP_Color_Create(123, 183, 220, 255);
 	background_colour = CP_Color_Create(233, 239, 255, 255);
 	white = CP_Color_Create(255, 255, 255, 255);
 
+	//calculate center of window
 	center_x = CP_System_GetWindowWidth() * 0.5f;
 	center_y = CP_System_GetWindowHeight() * 0.5f;
 
+	//get lowest score from leaderboard
 	lowest_score = GetLowestScore();
 
+	//set title position
 	title.x = center_x;
 	title.y = 100.0f;
 
+	//load leaderboard from file
 	errno_t err = fopen_s(&leaderboard_file, "Assets/leaderboard.txt", "r");
 	if (err != 0 || leaderboard_file == NULL) {
 		return;
@@ -86,16 +94,23 @@ void Leaderboard_Init(void) {
 			&leaderboard[j].score);
 	}
 	fclose(leaderboard_file);
+
+	//initialise bubbles
 	BubblesInit();
 }
 
 
 void Leaderboard_Update(void) {
+
+	//clear background
 	CP_Graphics_ClearBackground(background_colour);
 	CP_Image_Draw(background_image, (float)CP_System_GetWindowWidth() / 2, (float)CP_System_GetWindowHeight() / 2, (float)CP_System_GetWindowWidth(), (float)CP_System_GetWindowHeight(), 255);
 
+	//get mouse position
 	mx = CP_Input_GetMouseX();
 	my = CP_Input_GetMouseY();
+
+	//draw back button
 	int leaderboard_pop = 0;
 	CP_Settings_NoStroke();
 	if (IsAreaClicked(120, CP_System_GetWindowHeight() - 120, 100, 100, mx, my)) {
@@ -129,7 +144,7 @@ void Leaderboard_Update(void) {
 	CP_Settings_TextAlignment(horizontal, vertical);
 	CP_Font_DrawText("LEADERBOARD", title.x, title.y);
 
-	//draw table
+	//draw first leaderboard rectangle
 	CP_Settings_NoStroke();
 	CP_Settings_Fill(button_blue);
 	draw_leaderboard[0].width = 900.0f;
@@ -145,20 +160,23 @@ void Leaderboard_Update(void) {
 								draw_leaderboard[0].height,
 								draw_leaderboard[0].degrees,
 								draw_leaderboard[0].corner_radius);
-
+	//draw first leaderboard rank number
 	CP_Font_Set(title_font);
 	CP_Settings_TextSize(80.0f);
 	CP_Settings_TextAlignment(horizontal, vertical);
 	CP_Font_DrawText("1", 300, draw_leaderboard[0].y);
 
+	//draw first leaderboard name
 	CP_Settings_Fill(background_colour);
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_LEFT, vertical);
 	CP_Font_DrawText(leaderboard[0].name, 375, draw_leaderboard[0].y);
 
+	//draw first leaderboard score
 	sprintf_s(score_string, sizeof(score_string), "%d", leaderboard[0].score);
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_RIGHT, vertical);
 	CP_Font_DrawText(score_string, 1225, draw_leaderboard[0].y);
 
+	//draw rest of leaderboard entries
 	for (int i = 1; i < MAX_LEADERBOARD_ENTRIES; i++) {
 		CP_Settings_NoStroke();
 		CP_Settings_Fill(button_blue);
@@ -188,13 +206,14 @@ void Leaderboard_Update(void) {
 	}
 
 	// Draw back button
-
 	CP_Image_Draw(back_arrow, 120.0f, CP_System_GetWindowHeight() - 120.0f, 100.0f + leaderboard_pop, 100.0f + leaderboard_pop, 255);
+
+	//draw bubbles
 	Bubbles_Draw();
-
-
 }
+
 void Leaderboard_Exit(void) {
+	//free assets
 	CP_Font_Free(montserrat_light);
 	CP_Font_Free(title_font);
 	CP_Image_Free(back_arrow);
@@ -206,15 +225,18 @@ int GetLowestScore(void) {
 	int score = 0;
 	int lowest = INT_MAX;
 
+	//open leaderboard file
 	errno_t err = fopen_s(&leaderboard_file, "Assets/leaderboard.txt", "r");
 	if (err != 0 || leaderboard_file == NULL) {
 		return -1;
 	}
+	//read through file to find lowest score
 	while (fscanf_s(leaderboard_file, "%8s %d", name, (unsigned)(sizeof(name) / sizeof(name[0])), &score) == 2) {
 		if (score < lowest) {
 			lowest = score;
 		}
 	}
+	//close file and return lowest score
 	fclose(leaderboard_file);
 	return lowest;
 }
@@ -224,15 +246,21 @@ int GetHighestScore(void) {
 	char name[MAX_NAME_LENGTH + 1];
 	int score = 0;
 	int highest = 0;
+
+	//open leaderboard file
 	errno_t err = fopen_s(&leaderboard_file, "Assets/leaderboard.txt", "r");
 	if (err != 0 || leaderboard_file == NULL) {
 		return -1;
 	}
+
+	//read through file to find highest score
 	while (fscanf_s(leaderboard_file, "%8s %d", name, (unsigned)(sizeof(name) / sizeof(name[0])), &score) == 2) {
 		if (score > highest) {
 			highest = score;
 		}
 	}
+
+	//close file and return highest score
 	fclose(leaderboard_file);
 	return highest;
 }
--- src/core/mainmenu.c
+++ src/core/mainmenu.c
@@ -2,6 +2,7 @@
 @file		mainmenu.c
 @Author		Nur Nadia Natasya Binte Mohd Taha (nurnadianatasya.b@digipen.edu)
 @Co-authors	Lim Liang Zhou (l.liangzhou@digipen.edu)
+			Chiu Jun Wen (j.chiu@digipen.edu)
 @brief		This file contains the function definitions for the main menu,
 			including button interactions and navigation to other game states.
 			This file also includes bubble effects when buttons are clicked, and
